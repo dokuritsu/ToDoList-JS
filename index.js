bc40c0233d025f0191b5aa68e4aabe101b63158d
Laura Pereda <lypereda17@gmail.com>
2020-09-25 19:27:57 -0500

    able to submit new task and add to list

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function newTask() {
+  // Create a un ordered list
+  var li = document.createElement("li");
 
+  // Grab input
+  var inputVal = document.getElementById("enterTask").value;
+
+  // Create task & add to unordered list
+  var newTask = document.createElement("li");
+  newTask.innerText = inputVal;
+  li.appendChild(newTask);
+  document.getElementById("unorderedTasks").appendChild(li);
+
+  // Clear the input
+  document.getElementById("enterTask").value = "";
+}
