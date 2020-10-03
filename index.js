71a0a6568a6e4b8004a5c77382ad6f7fc160f289
Laura Pereda <lypereda17@gmail.com>
2020-10-03 16:16:05 -0500

    trying to add eventlisteners

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ function newTask() {
   // Grab input value from user
   var inputVal = document.getElementById("enterTask").value;
 
-  // Create task & add to unordered list
-  var newTask = document.createElement("li");
-  newTask.innerText = inputVal;
-  document.getElementById("todoTasks").appendChild(newTask);
+  // Check if value is empyu
+  if (inputVal === "") {
+    alert("Enter valid task!");
+  } else {
+    // Create task & add to unordered list
+    var newTask = document.createElement("li");
+    newTask.innerText = inputVal;
+    document.getElementById("todoTasks").appendChild(newTask);
+
+    // Add event listener to the new task
+    // newTask.addEventListener("click", finishedTask());
+  }
 
   // Clear the input
   document.getElementById("enterTask").value = "";
+}
+
+function finishedTask() {
+  alert("GJ u finished task!");
 }
