101017ba58f65f2a9ba2c0d8697a633e1202d2b1
Laura Pereda <lypereda17@gmail.com>
2020-10-03 15:25:34 -0500

    fixed double li creation in js

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 function newTask() {
-  // Create a list item
-  var li = document.createElement("li");
-
   // Grab input value from user
   var inputVal = document.getElementById("enterTask").value;
 
   // Create task & add to unordered list
   var newTask = document.createElement("li");
   newTask.innerText = inputVal;
-  li.appendChild(newTask);
-  document.getElementById("unorderedTasks").appendChild(li);
+  document.getElementById("todoTasks").appendChild(newTask);
 
   // Clear the input
   document.getElementById("enterTask").value = "";
